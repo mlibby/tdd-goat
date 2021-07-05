19f945ff51e59819d73ce3e7fc95060ef448e433
Michael C. Libby <m@mlibby.com>
2021-07-05 11:09:46 -0500

    expand func tests to input to-do-item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -8,12 +10,25 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
-        
+
     def test_start_list_retrieve_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        h1_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", h1_text)
 
+        input_box = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
+        input_box.send_keys("Buy peacock feathers")
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: But peacock feathers" for row in rows))
+
+        self.fail("Add more tests")
+        
 if __name__ == "__main__":
     unittest.main()
