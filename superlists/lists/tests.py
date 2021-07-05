f589375ff6e335515bf43f364f7223a8e3c1da9f
Michael C. Libby <m@mlibby.com>
2021-07-05 10:51:33 -0500

    first unit test, stub home page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(2 + 2, 5)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
