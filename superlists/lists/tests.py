1b5d38b31c484da765dcb84192de18f3eba57538
Michael C. Libby <m@mlibby.com>
2021-07-07 17:29:21 -0500

    add Item model

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -9,6 +9,11 @@ class HomePageTest(TestCase):
 
     def test_can_save_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual("A new list item", new_item.text)
+        
         self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
 
