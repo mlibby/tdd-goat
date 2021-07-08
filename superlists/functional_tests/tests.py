1f5779e7b6187a4c2caffb882b19673740de187a
Michael C. Libby <m@mlibby.com>
2021-07-07 21:55:23 -0500

    FT for multi-user lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,9 +15,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def assertRowTextInListTable(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_start_list_retrieve_later(self):
         self.browser.get(self.live_server_url)
@@ -29,17 +40,44 @@ class NewVisitorTest(LiveServerTestCase):
         new_item_input = self.browser.find_element_by_id("new_item_input")
         new_item_input.send_keys("Buy peacock feathers")
         new_item_input.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         self.assertRowTextInListTable("1: Buy peacock feathers")
 
         new_item_input = self.browser.find_element_by_id("new_item_input")
         new_item_input.send_keys("Use peacock feathers to make a fly")
         new_item_input.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         self.assertRowTextInListTable("1: Buy peacock feathers")
         self.assertRowTextInListTable("2: Use peacock feathers to make a fly")
 
-        self.fail("Add more tests")
+    def test_multiple_users_can_start_lists(self):
+        self.browser.get(self.live_server_url)
+        new_item_input = self.browser.find_element_by_id("new_item_input")
+        new_item_input.send_keys("Buy peacock feathers")
+        new_item_input.send_keys(Keys.ENTER)
+        self.assertRowTextInListTable("1: Buy peacock feathers")
 
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+
+        # new user visits
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # new visitor creates new list
+        new_item_input = self.browser.find_element_by_id("new_item_input")
+        new_item_input.send_keys("But milk")
+        new_item_input.send_keys(Keys.ENTER)
+        self.assertRowTextInListTable("1: Buy milk")
+
+        # new visitor has own list
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
