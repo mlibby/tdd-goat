ef452a636cb87a437a2d53b884d22f3ea26519ed
Michael C. Libby <m@mlibby.com>
2021-07-08 08:06:16 -0500

    separating list display to new view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,17 +7,6 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_can_save_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual("A new list item", new_item.text)
-
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/lists/the-only-list")
-
     def test_only_save_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
@@ -53,3 +42,17 @@ class ListViewTest(TestCase):
         response = self.client.get("/lists/the-only-list/")
         self.assertContains(response, "Item One")
         self.assertContains(response, "Item Two")
+
+
+class NewListTest(TestCase):
+    def test_can_save_POST_request(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual("A new list item", new_item.text)
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list")
+
+    
